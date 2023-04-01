64ea45fe3aaaa7eae2d601e54cf66cbf2d10d5b2
Kayem <abdoulatuf.pro@gmail.com>
2023-04-01 21:58:33 +0200

    feat: contact form

---
+++ src/Components/ContactForm.js
@@ -0,0 +1,58 @@
+import React, {useRef} from "react";
+import emailjs from "@emailjs/browser";
+
+const ContactForm = () => {
+  const form = useRef();
+
+  const formMess = document.querySelector(".form-message");
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_yaddjni",
+        "template_e01zwn9",
+        form.current,
+        process.env.REACT_APP_ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          form.current.reset();
+          formMess.innerHTML = "<p class='success'>Message envoyé !</p>";
+
+          setTimeout(() => {
+            formMess.innerHTML = "";
+          }, 2500);
+        },
+        (error) => {
+          console.log(error.text);
+          formMess.innerHTML =
+            "<p class='error'>Une erreur s'est produite, veuillez rééssayez</p>";
+
+          setTimeout(() => {
+            formMess.innerHTML = "";
+          }, 2500);
+        }
+      );
+  };
+
+  return (
+    <div className="form-container">
+        <h2>contactez-nous</h2>
+      <form ref={form} onSubmit={sendEmail} className="form-content">
+        <label>Nom</label>
+        <input type="text" name="name" required autoComplete="off" id="name" />
+        <label>Email</label>
+        <input type="email" name="email" required autoComplete="off" id="email" />
+        <label>Message</label>
+        <textarea name="message" required />
+        <input type="submit" value="Envoyer" className="hover button" />
+      </form>
+      <div className="form-message"></div>
+    </div>
+  );
+};
+
+export default ContactForm;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,65 @@
-import React from 'react';
+import React from "react";
+import Mouse from "../Components/Mouse";
+import Navigation from "../Components/Navigation";
+import Logo from "../Components/Logo";
+import ContactForm from "../Components/ContactForm";
+import CopyToClipboard from "react-copy-to-clipboard";
+import SocialNetwork from "../Components/SocialNetwork";
+import Buttons from "../Components/Buttons";
 
 const Contact = () => {
-    return (
-        <div>
-            <h1>Contact</h1>
+  return (
+    <main>
+      <Mouse />
+      <div className="contact">
+        <Navigation />
+        <Logo />
+        <ContactForm />
+        <div className="contact-infos">
+          <div className="address">
+            <div className="content">
+              <h4>adresse</h4>
+              <p>12 rue Laplace</p>
+              <p>64200 Biarritz</p>
+            </div>
+          </div>
+          <div className="phone">
+            <div className="content">
+              <h4>téléphone</h4>
+              <CopyToClipboard text="0601020304" className="hover">
+                <p
+                  style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => alert("Téléphone copié !")}
+                >
+                  06 01 02 03 04
+                </p>
+              </CopyToClipboard>
+            </div>
+          </div>
+          <div className="email">
+            <div className="content">
+              <h4>email</h4>
+              <CopyToClipboard text="htmlagency@gmail.com" className="hover">
+                <p
+                  style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => alert("Email copié !")}
+                >
+                  htmagency@gmail.com
+                </p>
+              </CopyToClipboard>
+            </div>
+          </div>
+          <SocialNetwork />
+          <div className="credits">
+            <p>hatem - 2023</p>
+          </div>
         </div>
-    );
+        <Buttons left={"/projet-4"} />
+      </div>
+    </main>
+  );
 };
 
 export default Contact;
