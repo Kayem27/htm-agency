d01b24bb973f206a2a03b4b47334d966a0a953b4
Kayem <abdoulatuf.pro@gmail.com>
2023-04-01 19:26:54 +0200

    add components:buttons && dynamique text

--- src/Components/Mouse.js
+++ src/Components/Mouse.js
@@ -8,6 +8,16 @@ const Mouse = () => {
         window.addEventListener("mousemove", (e) => {
             cursor.style.top = e.y + "px";
             cursor.style.left = e.x + "px";
+        });
+
+        document.querySelectorAll(".hover").forEach((link) => {
+            link.addEventListener('mouseover', () => {
+                cursor.classList.add('hovered')
+            })
+            link.addEventListener("mouseleave", () => {
+                cursor.style.transition = ".3s ease-out";
+                cursor.classList.remove('hovered');
+            })
         })
     }, [])
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,27 @@
 import React from "react";
 import Navigation from "../Components/Navigation";
 import Mouse from "../Components/Mouse";
+import SocialNetwork from "../Components/SocialNetwork";
+import DynamicText from "../Components/DynamicText";
+import Buttons from "../Components/Buttons";
 
 const Home = () => {
   return (
     <div>
+      <Mouse />
+      <div className="home">
         <Navigation />
-        <Mouse />
+        <SocialNetwork />
+        <div className="home-main">
+          <div className="main-content">
+            <h1>HTM AGENCY</h1>
+            <h2>
+              <DynamicText />
+            </h2>
+          </div>
+        </div>
+        <Buttons right={"/projet-1"} />
+      </div>
     </div>
   );
 };
